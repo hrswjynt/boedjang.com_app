49b72906783c911d4d6c0f120d6bd23c0c31508c
Boedjang Group <boedjanggroup@gmail.com>
2020-11-17 16:32:27 +0700

    asdsad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -280,7 +280,7 @@
             </div>
         </div>
         <!-- Content Row -->
-        <div class="row" style="margin-bottom: 30px">
+        <div class="row">
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-danger shadow h-100 py-2">
