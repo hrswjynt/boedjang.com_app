5724fd3b5e5838930387ab0fe664fbdb1fbe20d7
Boedjang Group <boedjanggroup@gmail.com>
2021-02-06 14:37:36 +0700

    sdssds

--- resources/views/sop/historyallprint.blade.php
+++ resources/views/sop/historyallprint.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laporan Bonus Karyawan </title>
+        <title>Laporan History Pembaca SOP </title>
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('assets/img/brand/boedjang.png')}}" type="image/png">
 
