0d16f616d4555ebc86047d162cb198ad5104aba4
Aulia Yayak <hahayayak@gmail.com>
2022-09-09 11:41:30 +0700

    bug fixing feedback search

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -126,7 +126,7 @@ class FeedbackController extends Controller
         $feedback = DB::table('feedback')->get();
         $data = DB::select(DB::raw("SELECT fdh.id,(CASE WHEN fdd.poin = 1 THEN 'Sangat Tidak Setuju' WHEN fdd.poin = 2 THEN 'Tidak Setuju' WHEN fdd.poin = 3 THEN 'Setuju' ELSE 'Sangat Setuju' END) as poin_nama, fdd.header_id, fdh.tgl, fdh.`user`, u.`name`, u.username as nip, fdh.outlet_name as outlet_nama, fdh.atasan, u2.`name` as atasan_nama, fdh.alasan1, fdh.alasan2, fdh.alasan3, fk.nama as kategori_nama, fdd.feedback as feedback_id, f.isi, fdd.poin, fdh.puas, absen.Cabang as cabang, absen.Jabatan as jabatan FROM u1127775_boedjang.feedback_data_detail fdd INNER JOIN u1127775_boedjang.feedback_data_header fdh ON fdd.header_id = fdh.id INNER JOIN u1127775_boedjang.feedback f on f.id = fdd.feedback INNER JOIN u1127775_boedjang.feedback_kategori fk on fk.id = f.kategori INNER JOIN u1127775_boedjang.users u on u.id = fdh.`user` INNER JOIN u1127775_boedjang.users u2 on u2.username = fdh.atasan INNER JOIN u1127775_absensi.Absen as absen on absen.NIP = u.username where date(fdh.tgl) between '" . $startdate . "' and '" . $enddate . "' ORDER BY fdh.id"));
 
-        return view('feedbacklaporan.index')->with('page', 'feedbacklaporan')->with('atasan', $atasan)->with('feedback', $feedback)->with('data', $data)->with('date1', $startdate)->with('date2', $enddate);
+        return view('feedbacklaporan.index')->with('page', 'feedbacklaporan')->with('feedback', $feedback)->with('data', $data)->with('date1', $startdate)->with('date2', $enddate);
     }
 
     public function delete($id)
