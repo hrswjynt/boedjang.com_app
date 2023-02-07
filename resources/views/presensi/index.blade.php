287827ca553c7df0f2df2485b30ee41c41adbe6f
Aulia Yayak <hahayayak@gmail.com>
2023-02-07 17:11:02 +0700

    presensi

--- resources/views/presensi/index.blade.php
+++ resources/views/presensi/index.blade.php
@@ -61,11 +61,11 @@
                                                 style="display: block;margin: 0 auto">Aktifkan Camera <i
                                                     class="fas fa-camera"></i></button>
                                             <video id="video" width="320" height="240" autoplay=""
-                                                style="display: none;margin: 3px"></video>
+                                                style="display: none;margin: 3px" class="img-thumbnail"></video>
                                             <button id="click-photo" style="display: none;margin: 0 auto" type="button"
                                                 class="btn btn-success">Ambil Foto <i class="fas fa-camera"></i></button>
                                             <div id="dataurl-container" style="display: none;margin-top:10px">
-                                                <canvas id="canvas" width="180" height="260" style="margin: 0 auto;display:block"></canvas>
+                                                <canvas id="canvas" width="180" height="260" style="margin: 0 auto;display:block" class="img-thumbnail"></canvas>
                                                 <textarea id="dataurl" readonly="" name="image" style="display: none"></textarea>
                                             </div>
                                         </div>
