4b395057ed43b33c65ef66586ec797e090bb63f8
Aulia Yayak <hahayayak@gmail.com>
2023-02-07 16:56:00 +0700

    presensi

--- resources/views/presensi/index.blade.php
+++ resources/views/presensi/index.blade.php
@@ -65,7 +65,7 @@
                                             <button id="click-photo" style="display: none;margin: 0 auto" type="button"
                                                 class="btn btn-success">Ambil Foto <i class="fas fa-camera"></i></button>
                                             <div id="dataurl-container" style="display: none;margin:3px">
-                                                <canvas id="canvas" width="320" height="500" style="margin: 0 auto;display:block"></canvas>
+                                                <canvas id="canvas" width="180" height="300" style="margin: 0 auto;display:block"></canvas>
                                                 <textarea id="dataurl" readonly="" name="image" style="display: none"></textarea>
                                             </div>
                                         </div>
