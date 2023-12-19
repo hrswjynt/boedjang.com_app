f079d99aa03a51f43658618ea37d2441290d6564
bebek-dev <73090765+bebek-dev@users.noreply.github.com>
2023-12-19 13:31:26 +0700

    Fix public path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
         //
         $this->app->bind('path.public', function() {
             // return '/home/samikary/admin.samikarya.com';
-            return 'C:/xampp/htdocs/boedjang.com_public';
+            return '/var/www/boedjangroup.com/boedjangroup-public.com';
         });
     }
 
